830ea5d335280cef5fa77c6040811beb2ca76fc7
skarokin <sean.kim0327@gmail.com>
2023-11-18 02:36:34 -0500

    fixed restart button

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,52 @@
 import React from "react";
-import { faker } from "@faker-js/faker";
+import GeneratedWords from "./components/GeneratedWords";
 import RestartButton from "./components/RestartButton";
 import TestResults from "./components/TestResults";
 import TypedCharacters from "./components/TypedCharacters";
 import useEngine from "./hooks/useEngine";
 import { calculateAccuracyPercentage } from "./utils/helpers";
 
-const words = faker.word.words(10);
+const App = () => {
+  const { words, typed, timeLeft, errors, state, restart, totalTyped } =
+    useEngine();
 
-function App() {
-  const { state, words, timeLeft, typed, errors, restart, totalTyped } = useEngine(); 
-  // the order of the components defines where they are rendered
   return (
     <>
-      <Countdown timeLeft = {timeLeft} />
-      {/* Overlay the typed characters onto the generated words; 
-          GeneratedWords and TypedCharacters share the same Tailwind properties */}
+      <CountdownTimer timeLeft={timeLeft} />
       <WordsContainer>
-        <GeneratedWords words = {words} />
-        {/* pass the generated words into TypedCharacters */}
-        <TypedCharacters 
-          className="absolute inset-0" 
-          words={words} 
-          userInput = {typed}
+        <GeneratedWords key={words} words={words} />
+        {/* User typed characters will be overlayed over the generated words */}
+        <TypedCharacters
+          className="absolute inset-0"
+          words={words}
+          userInput={typed}
         />
       </WordsContainer>
       <RestartButton
         className={"mx-auto mt-10 text-slate-500"}
-        onRestart={() => null}
+        onRestart={restart}
       />
       <TestResults
         className="mt-10"
+        state={state}
         errors={errors}
         accuracyPercentage={calculateAccuracyPercentage(errors, totalTyped)}
         total={totalTyped}
-        state={state}
       />
     </>
   );
-}
+};
 
-const WordsContainer = ({ children }: {children: React.ReactNode}) => {
+const WordsContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative max-w-xl mt-3 text-3xl leading-relaxed break-all">
+    <div className="relative text-3xl max-w-xl leading-relaxed break-all mt-3">
       {children}
     </div>
-  )
+  );
 };
 
-// this function takes string param "words" and returns the words to show on the screen
-const GeneratedWords = ({ words }: { words: string }) => {
-  return <div className="text-blue-200">
-    {words}
-  </div>;
-};
-
-// this function takes number parameter "timeLeft" and returns the timeLeft
-const Countdown = ({ timeLeft }: {timeLeft: number}) => {
-  return <h2 className="text-primary-300 font-medium"> Time: { timeLeft }
-  </h2>
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
 };
 
 export default App;
